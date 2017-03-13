41481189454bd8eae2dc0ac09c0ea8c405f6f796
Jill Magsaysay <jill@mondorobot.com>
2017-03-13 11:33:30 -0600

    Server updates

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var path = require('path');
-var serveStatic = require('serve-static');
 
 app = express();
-app.use(serveStatic(__dirname));
+app.use(express.static(`${__dirname}/dist`));
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port);
